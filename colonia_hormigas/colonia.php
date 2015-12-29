b08e56aeda7d61cd993b2435985545b4170f1630
jmanuoz <jmanuoz@gmail.com>
2015-12-29 18:12:09 -0300

    aaa

--- colonia_hormigas/colonia.php
+++ colonia_hormigas/colonia.php
@@ -4,13 +4,13 @@ require_once 'hormiga.php';
 require_once 'feromonas.php';
 Class Colonia_hormigas{
     
-    public function resolver($inicio){
+    public function resolver($inicio, $proba=2){
         $feromonas = New Feromonas();
        
         for($i= 1;$i<400;$i++){
             $sudoku = new Sudoku($inicio);
             
-            $hormiga = new Hormiga($sudoku,$feromonas,2);
+            $hormiga = new Hormiga($sudoku,$feromonas,$proba);
             
             if($i % 6 == 0){                
                $feromonas->evaporar();    
@@ -18,7 +18,7 @@ Class Colonia_hormigas{
             }
             
             if($hormiga->resolver_por_posibles()){
-                echo 'Hormiga: '.$i.'<br>';
+               // echo 'Hormiga: '.$i.'<br>';
                 return true;
     //            echo '<br>Inicial:';
     //            $sudoku = new Sudoku($random);
--- hormigas.php
+++ hormigas.php
@@ -1,19 +1,27 @@
 <?php
-ini_set('max_execution_time', 300);
+ini_set('max_execution_time', 900);
 require_once 'inicios.php';
 require_once 'colonia_hormigas/colonia.php';
 
 $cont1= $cont2= 0;
 $colonia = new Colonia_hormigas();
-
-
-for($a=0;$a<30;$a++){
+$funciones = ['inicio1', 'inicio2', 'inicio3', 'inicioDiagonal', 'inicioDiagonalcita', 'inicioDiagonalcita2',
+				'inicioCruzado','inicio1A', 'inicio4A', 'inicioEasyA', 'inicioEasyB', 'inicioEasyC', 'inicioEasyE', 'inicioEasyE'
+				];
+foreach ($funciones as $fun) {
     
-    $random = inicio4A();
-    //echo '<pre>';print_r($random);echo '</pre>';exit;
-    if($colonia->resolver($random)){
-        $cont1++;
+    for($a=0;$a<30;$a++){
+
+
+        //echo '<pre>';print_r($random);echo '</pre>';exit;
+        if($colonia->resolver(call_user_func($fun),$a)){
+
+            $cont1++;
+        }
+        echo $fun.','.$a.','.$cont1.'<br>';
+        $cont1=0;
     }
+    
 }
 
-echo 'Sin recalcular: '.$cont1;
+echo 'Resueltos '.$cont1;
